8ad65df5bd0224fa47fcf8ba9d2b9310a57786a1
Shiho.Komuro <shiho.komuro@rakuten.com>
2018-06-14 03:19:36 +0000

    user login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
         @include('commons.navbar')
         
         <div class="container">
+            @include('commons.error_messages')
             
             @yield('content')
         </div>
